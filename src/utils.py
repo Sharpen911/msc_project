424844cf73af65cf2330f33619407de557d1ca85
Sharpen <635023135@qq.com>
2020-06-05 14:25:33 +0100

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,9 @@ ACCESS_TOKEN_SECRET = "tK5NyYgtLnDLxI786h8uT9kRtPoc3UzNsED9YwcPhItvW"
 CONSUMER_KEY = "qqpnwdem9wn7l3nplnFQY71jE"
 CONSUMER_SECRET = "g9Z8DvPpSIUxdtY1xqyuvzrUfimPJ6TvQE4hXz1kTMFW7kxzAv"
 
-data_path = 'sample_data/'
+data_path = 'data/'
 
 save_dir = 'collected_tweets/'
+
+cities = ['joh']
+
