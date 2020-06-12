3b67af54e2bc33fdfdec645affbd0b5b34184899
Peng Xiao <s1931563@hawksworth.inf.ed.ac.uk>
2020-06-12 21:29:52 +0100

    update finished

--- src/update_tweets.py
+++ src/update_tweets.py
@@ -6,48 +6,48 @@ new_path ='/disk/data/share/s1931563/msc_project/src/collected_tweets/'
 
 old_path = '/disk/data/share/s1931563/tweet_data/'
 
-cities = ['joh']
- 
-for city in cities:
-    old_tweets = listdir(old_path+'tweets_'+city)
-    new_tweets = listdir(new_path+city)
-
-    intersec = set.intersection(set(old_tweets), set(new_tweets))
-    intersec = sorted(intersec)
-    for user_csv in intersec:
-        new_csv_path = new_path + city + '/' + user_csv
-        old_csv_path = old_path + 'tweets_'+city +'/'+ user_csv
-        
-
-        try:
-            new = pd.read_csv(new_csv_path,usecols = ['created_at'],lineterminator = '\n')
-
-            if dt.strptime(str(new.iloc[-1].values[0]), "%Y-%m-%d %H:%M:%S") >= dt.strptime("2019-01-01 06:23:04", "%Y-%m-%d %H:%M:%S"):
-
-                old = pd.read_csv(old_csv_path,lineterminator = '\n')
-                old.to_csv(new_csv_path, mode='a', header=False, index=False)
-
-        except:
-            print('skip the user {}'.format(user_csv))
-
-
-
-# for city in cities:
-#     df = pd.read_csv('/disk/data/share/s1931563/msc_project/src/user_data/' + city+'.csv')
-#     all_ids = df.user_id.tolist()
+cities = ['joh','lon','nyc','ran']
 # 
-#     ideal_tweets = [str(id)+'_tweets.csv' for id in all_ids]
-#     old_tweets = listdir(old_path+'tweets_'+city)
-#     new_tweets = listdir(new_path + city)
+#for city in cities:
+#    old_tweets = listdir(old_path+'tweets_'+city)
+#    new_tweets = listdir(new_path+city)
 #
+#    intersec = set.intersection(set(old_tweets), set(new_tweets))
+#    intersec = sorted(intersec)
+#    for user_csv in intersec:
+#        new_csv_path = new_path + city + '/' + user_csv
+#        old_csv_path = old_path + 'tweets_'+city +'/'+ user_csv
+#        
 #
+#        try:
+#            new = pd.read_csv(new_csv_path,usecols = ['created_at'],lineterminator = '\n')
 #
-#     to_be_moved = set.intersection(set(ideal_tweets),set(old_tweets)) - set(new_tweets)
+#            if dt.strptime(str(new.iloc[-1].values[0]), "%Y-%m-%d %H:%M:%S") >= dt.strptime("2019-01-01 06:23:04", "%Y-%m-%d %H:%M:%S"):
 #
-#     for user_csv in to_be_moved:
-#         new_csv_path = new_path + city + '/' + user_csv
-#         old_csv_path = old_path + 'tweets_' + city + '/' + user_csv
-#         shutil.copyfile(old_csv_path,new_csv_path)
+#                old = pd.read_csv(old_csv_path,lineterminator = '\n')
+#                old.to_csv(new_csv_path, mode='a', header=False, index=False)
+#
+#        except:
+#            print('skip the user {}'.format(user_csv))
+
+
+
+for city in cities:
+    df = pd.read_csv('/disk/data/share/s1931563/msc_project/src/user_data/' + city+'.csv')
+    all_ids = df.user_id.tolist()
+
+    ideal_tweets = [str(id)+'_tweets.csv' for id in all_ids]
+    old_tweets = listdir(old_path+'tweets_'+city)
+    new_tweets = listdir(new_path + city)
+
+
+
+    to_be_moved = set.intersection(set(ideal_tweets),set(old_tweets)) - set(new_tweets)
+    print(len(to_be_moved))
+    for user_csv in to_be_moved:
+        new_csv_path = new_path + city + '/' + user_csv
+        old_csv_path = old_path + 'tweets_' + city + '/' + user_csv
+        shutil.copyfile(old_csv_path,new_csv_path)
 
 
 
