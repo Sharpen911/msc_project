2a6c082f398f0786ca0a4774474d252de41e5c21
Peng Xiao <s1931563@hawksworth.inf.ed.ac.uk>
2020-06-20 22:04:36 +0100

    small edit

--- src/update_tweets.py
+++ src/update_tweets.py
@@ -24,7 +24,7 @@ for city in cities:
            #new = pd.read_csv(new_csv_path,usecols = ['created_at'],lineterminator = '\n')
            new = pd.read_csv(new_csv_path)
            old = pd.read_csv(old_csv_path)
-
+           
            if dt.strptime(str(new.created_at.iloc[-1]), "%Y-%m-%d %H:%M:%S") >= dt.strptime("2019-01-01 06:23:04", "%Y-%m-%d %H:%M:%S"):
                merged = pd.concat([new,old])
                merged.to_csv(new_csv_path,index=False)
@@ -33,26 +33,26 @@ for city in cities:
 # old.to_csv(new_csv_path, mode='a', header=False, index=False)
 
        except:
-           print('skip the user {}'.format(user_csv))
+            print('skip the user {}'.format(user_csv))
 
 
-
-for city in cities:
-    df = pd.read_csv('/disk/data/share/s1931563/msc_project/src/user_data/' + city+'.csv')
-    all_ids = df.user_id.tolist()
-
-    ideal_tweets = [str(id)+'_tweets.csv' for id in all_ids]
-    old_tweets = listdir(old_path+'tweets_'+city)
-    new_tweets = listdir(new_path + city)
-
-
-
-    to_be_moved = set.intersection(set(ideal_tweets),set(old_tweets)) - set(new_tweets)
-    print(len(to_be_moved))
-    for user_csv in to_be_moved:
-        new_csv_path = new_path + city + '/' + user_csv
-        old_csv_path = old_path + 'tweets_' + city + '/' + user_csv
-        shutil.copyfile(old_csv_path,new_csv_path)
-
+#
+#for city in cities:
+#    df = pd.read_csv('/disk/data/share/s1931563/msc_project/src/user_data/' + city+'.csv')
+#    all_ids = df.user_id.tolist()
+#
+#    ideal_tweets = [str(id)+'_tweets.csv' for id in all_ids]
+#    old_tweets = listdir(old_path+'tweets_'+city)
+#    new_tweets = listdir(new_path + city)
+#
+#
+#
+#    to_be_moved = set.intersection(set(ideal_tweets),set(old_tweets)) - set(new_tweets)
+#    print(len(to_be_moved))
+#    for user_csv in to_be_moved:
+#        new_csv_path = new_path + city + '/' + user_csv
+#        old_csv_path = old_path + 'tweets_' + city + '/' + user_csv
+#        shutil.copyfile(old_csv_path,new_csv_path)
+#
 
 
